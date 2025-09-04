fa1c03bbf4394fdef890f8de815c1e7f27c695b3
ujjawal <ujjawal17032002@gmail.com>
2025-09-05 05:21:18 +0530

    main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def hello_world():
+    return {"message": "hello, world"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
